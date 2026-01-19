02dbcd42d746be4f8e57517bfb74b3e797768862
sryang <sarang628@gmail.com>
2026-01-19 09:42:02 +0900

    change restaurant list action button position

--- FindScreenModule.kt
+++ FindScreenModule.kt
@@ -3,8 +3,10 @@ package com.sarang.torang.di.finding_di
 import android.Manifest
 import android.content.Context
 import android.location.Location
+import android.util.Log
 import androidx.annotation.RequiresPermission
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
@@ -30,6 +32,7 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.tooling.preview.Preview
@@ -178,6 +181,7 @@ fun IntergratedFind(
     var cardPagerHeight     : Int                           by remember { mutableIntStateOf(0) }
     val cardPagerHeightDp   : Dp                            = with(LocalDensity.current){ cardPagerHeight.toDp() }
     val usePreciseLocation  : Boolean                       = true
+    val density = LocalDensity.current
 
     val filter : @Composable () -> Unit = {
         Filter(
@@ -222,10 +226,10 @@ fun IntergratedFind(
                 onClickCard         = { navController.restaurant(it) },
                 visible             = isVisible,
                 onPosition          = { lat,lon-> moveCamera(coroutineScope = coroutineScope,
-                                                             cameraPositionState = cameraPositionState,
-                                                             latitude =  lat,
-                                                             longitude = lon,
-                                                             zoom = 17f) },
+                    cameraPositionState = cameraPositionState,
+                    latitude =  lat,
+                    longitude = lon,
+                    zoom = 17f) },
                 onChangePage        = { if(isVisible) onChangePage.invoke(it) },
                 focusedRestaurant   = cardFocusedRestaurant
             )
@@ -289,7 +293,8 @@ fun IntergratedFind(
                 modifier = Modifier
                     .size(66.dp)
                     .padding(16.dp)
-                    .align(if (isVisible) Alignment.CenterEnd else Alignment.BottomEnd),
+                    .offset(y = -cardPagerHeightDp )
+                    .align(Alignment.BottomEnd),
                 shape    = CircleShape,
                 onClick  = { coroutineScope.launch {
                                 if(findState.bottomSheetState.bottomSheetState.currentValue != SheetValue.Expanded)
