5663f218e185f9ca9f4093379b3162bccc6d3f82
sryang <sarang628@gmail.com>
2026-02-09 23:36:13 +0900

    add video review

--- CustomCardInfoImageLoader.kt
+++ CustomCardInfoImageLoader.kt
@@ -1,7 +1,17 @@
 package com.sarang.torang.di.finding_di
 
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.unit.dp
 import com.sarang.torang.compose.FilterImageLoader
 import com.sarang.torang.compose.cardinfo.CardInfoImageLoader
+import com.sarang.torang.di.image.TorangAsyncImageData
 import com.sarang.torang.di.image.provideTorangAsyncImage
 
-val customImageLoader: CardInfoImageLoader = { modifier, url, width, height, scale -> provideTorangAsyncImage().invoke(modifier, url, width, height, scale)  }
+val customImageLoader: CardInfoImageLoader = { modifier, url, width, height, scale -> provideTorangAsyncImage().invoke(
+    TorangAsyncImageData(
+        modifier = modifier,
+        model = url,
+        progressSize = width ?: 30.dp,
+        errorIconSize = height ?: 30.dp,
+        contentScale = scale ?: ContentScale.None)
+)  }
--- CustomFilterImageLoader.kt
+++ CustomFilterImageLoader.kt
@@ -1,6 +1,18 @@
 package com.sarang.torang.di.finding_di
 
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.unit.dp
 import com.sarang.torang.compose.FilterImageLoader
+import com.sarang.torang.di.image.TorangAsyncImageData
 import com.sarang.torang.di.image.provideTorangAsyncImage
 
-val filterImageLoader: FilterImageLoader = { modifier, url, width, height, scale -> provideTorangAsyncImage().invoke(modifier, url, width, height, scale) }
+val filterImageLoader: FilterImageLoader = { modifier, url, width, height, scale -> provideTorangAsyncImage().invoke(
+    TorangAsyncImageData(
+        modifier = modifier,
+        model = url,
+        progressSize = width ?: 30.dp,
+        errorIconSize = height ?: 30.dp,
+        contentScale = scale ?: ContentScale.None
+    )
+)
+}
